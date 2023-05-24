6709b705c8201a02128fe7ad57642018111aa27f
timkn <timknothe21@gmail.com>
2023-05-24 15:09:27 +0200

    added poppins font

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,7 +1,13 @@
 <script>
     import Nav from "$lib/Nav.svelte";
     import Footer from "$lib/Footer.svelte";
+    import "@fontsource/poppins";
 </script>
+
+<style lang="sass">
+    *
+      font-family: Poppins, sans-serif
+</style>
 
 
 <Nav/>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -6,6 +6,13 @@
 </script>
 
 
+<style lang="sass">
+    *
+      font-family: Poppins, sans-serif
+
+</style>
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <Button>Test</Button>
