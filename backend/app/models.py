a6a75643f63a6a222c488f840923714b376c18bb
timkn <timknothe21@gmail.com>
2023-05-30 07:44:12 +0200

    uodated model and schema

--- backend/app/models.py
+++ backend/app/models.py
@@ -9,39 +9,25 @@ class Hotel(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String)
     stars = Column(Integer)
-    rooms = relationship("Room", back_populates="hotel")
-
-
-class Flight(Base):
-    __tablename__ = 'flights'
-
-    id = Column(Integer, primary_key=True)
-    departure_airport = Column(String)
-    arrival_airport = Column(String)
-    departure_datetime = Column(DateTime)
-    arrival_datetime = Column(DateTime)
-    offers = relationship("Booking", back_populates="flight")
 
 
 class Offer(Base):
     __tablename__ = 'offers'
-
     id = Column(Integer, primary_key=True)
     hotel_id = Column(Integer, ForeignKey('hotels.id'))
-    flight_id = Column(Integer, ForeignKey('flights.id'))
-    count_adults = Column(Integer)
-    count_children = Column(Integer)
+    outbounddeparturedatetime = Column(DateTime)
+    inbounddeparturedatetime = Column(DateTime)
+    countadults = Column(Integer)
+    countchildren = Column(Integer)
     price = Column(Float)
-    meal_type = Column(Enum("BREAKFAST", "ALLINCLUSIVE", "HALFBOARD", "NONE", name="meal_type_enum"), nullable=False)
+    inbounddepartureairport = Column(String)
+    inboundarrivalairport = Column(String)
+    inboundarrivaldatetime = Column(DateTime)
+    outbounddepartureairport = Column(String)
+    outboundarrivalairport = Column(String)
+    outboundarrivaldatetime = Column(DateTime)
+    meal_type = Column(String)
+    oceanview = Column(Boolean)
+    roomtype = Column(String)
     hotel = relationship("Hotel", back_populates="offers")
-    flight = relationship("Flight", back_populates="offers")
 
-
-class Room(Base):
-    __tablename__ = 'rooms'
-
-    id = Column(Integer, primary_key=True)
-    hotel_id = Column(Integer, ForeignKey('hotels.id'))
-    type = Column(Enum("APARTMENT", "DOUBLE", "STUDIO", "FAMILY", "SUITE", "SINGLE", "ACCORDINGDESCRIPTION", name="room_type_enum"))
-    ocean_view = Column(Boolean)
-    offers = relationship("Booking", back_populates="room")
--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,42 +1,9 @@
 from datetime import datetime
-from enum import Enum
-from typing import Optional
 from pydantic import BaseModel
 
 
-class MealTypeEnum(str, Enum):
-    BREAKFAST = 'Breakfast'
-    LUNCH = 'Lunch'
-    DINNER = 'Dinner'
-
-
-class BookingBase(BaseModel):
-    hotel_id: int
-    flight_id: int
-    count_adults: int
-    count_children: int
-    price: float
-    meal_type: Optional[MealTypeEnum]
-
-
-class BookingCreate(BookingBase):
-    pass
-
-
-class BookingUpdate(BookingBase):
-    pass
-
-
-class Booking(BaseModel):
-    id: int
-    hotel: "Hotel"
-    flight: "Flight"
-
-    class Config:
-        orm_mode = True
-
-
 class HotelBase(BaseModel):
+    id: int
     name: str
     stars: int
 
@@ -45,69 +12,35 @@ class HotelCreate(HotelBase):
     pass
 
 
-class HotelUpdate(HotelBase):
-    pass
-
-
-class Hotel(BaseModel):
-    id: int
-    stars: int
-    name: str
-    rooms: list["Room"]
-    offers: list[Booking]
-
+class Hotel(HotelBase):
     class Config:
         orm_mode = True
 
 
-class FlightBase(BaseModel):
-    departure_airport: str
-    arrival_airport: str
-    departure_datetime: datetime
-    arrival_datetime: datetime
-
-
-class FlightCreate(FlightBase):
-    pass
-
-
-class FlightUpdate(FlightBase):
-    pass
-
-
-class Flight(BaseModel):
-    id: int
-    departure_airport: str
-    arrival_airport: str
-    departure_datetime: datetime
-    arrival_datetime: datetime
-    offers: list[Booking]
-
-    class Config:
-        orm_mode = True
-
-
-class RoomBase(BaseModel):
+class OfferBase(BaseModel):
     hotel_id: int
-    type: str
-    ocean_view: Optional[bool]
+    outbounddeparturedatetime: datetime
+    inbounddeparturedatetime: datetime
+    countadults: int
+    countchildren: int
+    price: float
+    inbounddepartureairport: str
+    inboundarrivalairport: str
+    inboundarrivaldatetime: datetime
+    outbounddepartureairport: str
+    outboundarrivalairport: str
+    outboundarrivaldatetime: datetime
+    meal_type: str
+    oceanview: bool
+    roomtype: str
 
 
-class RoomCreate(RoomBase):
+class OfferCreate(OfferBase):
     pass
 
 
-class RoomUpdate(RoomBase):
-    pass
-
-
-class Room(BaseModel):
+class Offer(OfferBase):
     id: int
-    hotel_id: int
-    type: str
-    ocean_view: Optional[bool]
-    hotel: Hotel
-    offers: list[Booking]
 
     class Config:
         orm_mode = True
