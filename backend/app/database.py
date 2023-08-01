fa2ef633fa86cf8eb886405c46e67c3a569d1649
timkn <timknothe21@gmail.com>
2023-08-01 15:14:37 +0000

    used env vars

--- backend/app/database.py
+++ backend/app/database.py
@@ -5,7 +5,13 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 
-SQLALCHEMY_DATABASE_URL = os.environ['DATABASE_URL']
+POSTGRES_USER = os.environ['POSTGRES_USER']
+POSTGRES_PASSWORD = os.environ['POSTGRES_PASSWORD']
+POSTGRES_DB = os.environ['POSTGRES_DB']
+POSTGRES_PORT = os.environ['POSTGRES_PORT']
+POSTGRES_HOST = os.environ['POSTGRES_HOST']
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
