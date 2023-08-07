204e22d83e1e0f156a90da18048cb7b9b0946f80
timkn <timknothe21@gmail.com>
2023-08-07 01:00:55 +0000

    added docker

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,6 +1,6 @@
 import preprocess from "svelte-preprocess";
 import { vitePreprocess } from "@sveltejs/kit/vite";
-import adapter from "@sveltejs/adapter-cloudflare";
+import adapter from "@sveltejs/adapter-node";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -17,13 +17,7 @@ const config = {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter({
-      // See below for an explanation of these options
-      routes: {
-        include: ["/*"],
-        exclude: ["<all>"],
-      },
-    }),
+    adapter: adapter(),
   },
 };
 
